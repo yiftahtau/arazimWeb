cd45b21712f542be28625818d306d33d5bf1a2cc
Yiftah <yiftahbaruch@mail.tau.ac.il>
2023-06-06 18:57:01 +0300

    Hello Jinja

--- HelloWeb.py
+++ HelloWeb.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from jinja2 import Environment, PackageLoader, select_autoescape
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-   htmlFile = open('webPage.html', 'r')
-   content = htmlFile.read()
-   htmlFile.close()
-   return content
+   username = request.args.get("name")
+   if username == None:
+      htmlFile = open('loginPage.html', 'r')
+      content = htmlFile.read()
+      htmlFile.close()
+      return content
+   helloPage = open('webPage.html', 'r')
+   content = helloPage.read()
+   environment = Environment()
+   template = environment.from_string(content)
+   return template.render(name=username)
 
 if __name__ == '__main__':
    app.run()
